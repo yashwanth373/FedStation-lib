a89c564f145987a57f1d2c18785080052902cd0e
karthik <karthik.pasupulatei@gmail.com>
2022-04-19 14:03:11 +0530

    server integrations done

--- src/Fedstation/RecieveModel.py
+++ src/Fedstation/RecieveModel.py
@@ -1,11 +1,12 @@
 import shutil
+import sys
 import requests
 import pickle
 import numpy
 
 
 MODEL_PICKEL_FILENAME = "toxic_msgs_logistic_regression_and_vector.pkl"
-
+PROJECT_ID = sys.argv[1]
 def getModel(): 
         #retrieves the model from the pickle file 
         #and returns the Model 
@@ -16,8 +17,7 @@ def getModel():
         #if pickle file not found or empty 
 def recieveModelFromServer(): 
         #recieve model from server
-        search_api_url = "http://127.0.0.1:8000/getGlobalModelFile/k_k"
-
+        search_api_url = "http://127.0.0.1:8000/getGlobalModelFile/" + PROJECT_ID
         
         with requests.get(search_api_url, stream=True) as r:
             with open(MODEL_PICKEL_FILENAME, 'wb') as f:
